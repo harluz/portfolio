6fa3d3b05e2d68c5a0d6a8877322c8666e45b2fe
harluz <harluz06@gmail.com>
2023-03-19 14:04:36 +0900

    [add] docker+rails6+mysqlの環境及びjquery+bootstrap+rspec+rubocop+haml+I18nのgemを追加し動作確認を行なったもの

---
+++ spec/requests/test_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Test", type: :request do
+  describe 'GET /test' do
+    subject(:result) { get test_path }
+
+    it 'アクセスできること' do
+      expect(result).to eq 200
+    end
+  end
+end
