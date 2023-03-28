b2f3566157ce45e005c4d7ca64ab6c3afa18aea3
harluz <harluz06@gmail.com>
2023-03-28 23:02:34 +0900

    [add] deviseで生成されたuserのmigrateファイルをポートフォリオ用に調整したもの

--- db/migrate/20230328123738_devise_create_users.rb
+++ db/migrate/20230328123738_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
+      t.integer :havin_xp, null: false, default: 0
+      t.integer :level, null: false, default: 0
+      t.integer :challenge_id
+
 
       ## Recoverable
       t.string   :reset_password_token
