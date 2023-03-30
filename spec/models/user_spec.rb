f04cfbf309ab89b2e44ea3dd11fedab9e4a4eb2e
harluz <harluz06@gmail.com>
2023-03-31 00:00:30 +0900

    [clean] テストをグルーピングしたものかつ今後リレーションのテストを追加するようコメントを残したもの

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,15 +5,19 @@ RSpec.describe User, type: :model do
   let(:non_correct_user) { build(:non_correct_user) }
   let(:duplicate_user) { build(:duplicate_user) }
 
-  it "ユーザーの情報が有効であること" do
-    expect(correct_user).to be_valid
-  end
+  describe "バリデーションのテスト" do
+    it "ユーザーの情報が有効であること" do
+      expect(correct_user).to be_valid
+    end
 
-  it "ユーザーの情報が無効であること" do
-    expect(non_correct_user).not_to be_valid
-  end
+    it "ユーザーの情報が無効であること" do
+      expect(non_correct_user).not_to be_valid
+    end
 
-  it "ユーザーのemailが登録済みのemailと重複していることから無効であること" do
-    expect(duplicate_user).not_to be_valid
+    it "ユーザーのemailが登録済みのemailと重複していることから無効であること" do
+      expect(duplicate_user).not_to be_valid
+    end
   end
+  # describe "アソシエーションのテスト" do
+  # end
 end
