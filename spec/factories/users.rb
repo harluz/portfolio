8eafbc143c9889f2b5c030515b944a1c7de59cd3
harluz <harluz06@gmail.com>
2023-03-30 22:48:34 +0900

    [update] ユーザーのfactoryに正常なuserとnon_correct_user duplicate_userwo

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,19 @@
 FactoryBot.define do
   factory :user do
+    name { "sample user" }
+    email { "sample@mail.com" }
+    password { "samplepassword" }
+  end
+
+  factory :non_correct_user, class: User do
+    name { "" }
+    email { "samplemailcom" }
+    password { "pass" }
+  end
+
+  factory :duplicate_user, class: User do
+    name { "sample user2" }
+    email { "sample@mail.com" }
+    password { "samplepassword2" }
   end
 end
