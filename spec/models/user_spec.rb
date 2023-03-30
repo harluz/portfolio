ec706e4c8c8f2132cc464a3937d21cb9e8bd5171
harluz <harluz06@gmail.com>
2023-03-30 23:04:34 +0900

    [update] ユーザーのモデルスペックを実装したもの

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:correct_user) { create(:user) }
+  let(:non_correct_user) { build(:non_correct_user) }
+  let(:duplicate_user) { build(:duplicate_user) }
+
+  it "ユーザーの情報が有効であること" do
+    expect(correct_user).to be_valid
+  end
+
+  it "ユーザーの情報が無効であること" do
+    expect(non_correct_user).not_to be_valid
+  end
+
+  it "ユーザーのemailが登録済みのemailと重複していることから無効であること" do
+    expect(duplicate_user).not_to be_valid
+  end
 end
