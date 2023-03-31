e3b02801b053a004c144b0c12aede20355006eab
harluz <harluz06@gmail.com>
2023-03-31 21:25:42 +0900

    [update] タイムゾーンを東京に変更し日本時間に設定したもの

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     config.load_defaults 6.1
 
