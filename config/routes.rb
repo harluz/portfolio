eb413c3c4b4bf78dacf37b50cbff4e400208cfab
harluz <harluz06@gmail.com>
2023-04-01 11:47:24 +0900

    [fix] ユーザー登録失敗時に画面をリロードした際にページが見つからないエラーを修正したもの

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#top'
   get 'pages/top'
+  get 'pages/main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+  end
+  
   get 'test', to: 'test#index'
 end
