20a72e317b0b767bfdbf750b87f7c7f9bc8f96bf
harluz <harluz06@gmail.com>
2023-04-02 08:58:43 +0900

    [update] ユーザー登録及びログインのリクエストスペックを実装したもの

--- spec/requests/user_registrations_spec.rb
+++ spec/requests/user_registrations_spec.rb
@@ -2,9 +2,59 @@ require 'rails_helper'
 
 RSpec.describe "UserRegistrations", type: :request do
   describe "GET /user_registrations" do
-    it "テスト疎通確認" do
+    it "ユーザー登録ページのgetリクエストが成功していること" do
       get new_user_registration_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "登録情報が正しい場合" do
+    it "サインアップに成功した際にユーザー数が増加していること" do
+      expect do
+        post user_registration_path, params: { user: attributes_for(:user) }
+      end.to change(User, :count).by(1)
+    end
+
+    describe "レスポンス確認" do
+      before { post user_registration_path, params: { user: attributes_for(:user) } }
+
+      it "〇〇ページにリダイレクトされていること" do
+        expect(response).to redirect_to pages_main_path
+      end
+
+      it "ステータスコード303（リダイレクト）がレスポンスされていること" do
+        expect(response).to have_http_status(303)
+      end
+
+      it "許可されているページにアクセスした際、ステータスコード200がレスポンスされていること" do
+        get pages_main_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    # 許可されたページにアクセスした際、ステータスコード200（）がレスポンスされていること
+    # サインアップした場合に表示されるヘッダーのレスポンスが含まれていること
+    
+  end
+
+  describe "登録内容が正しくない場合" do
+    it "ユーザー数が変化していないこと" do
+      expect do
+        post user_registration_path, params: { user: attributes_for(:non_correct_user) }
+      end.to change(User, :count).by(0)
+    end
+
+    describe "レスポンス確認" do
+      before { post user_registration_path, params: { user: attributes_for(:non_correct_user) } }
+
+      it "ステータスコード422(バリデーションエラー)がレスポンスされていること" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "許可されていないページにアクセスした際、ステータスコード302（リダイレクト）がレスポンスされていること" do
+        get pages_main_path
+        expect(response).to have_http_status(302)
+      end
     end
   end
 end
--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -2,9 +2,48 @@ require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :request do
   describe "GET /user_sessions" do
-    it "テスト疎通確認" do
+    it "ログインページのgetリクエストが成功していること" do
       get new_user_session_path
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "ログインに成功する場合" do
+    let!(:user) { create(:user) }
+    
+    before do
+      valid_params = { email: user.email, password: user.password }
+      post user_session_path, params: { user: valid_params } 
+    end
+
+    it "〇〇ページにリダイレクトされていること" do
+      expect(response).to redirect_to pages_main_path
+    end
+
+    it "ステータスコード303（リダイレクト）がレスポンスされていること" do
+      expect(response).to have_http_status(303)
+    end
+
+    it "許可されているページにアクセスした際、ステータスコード200がレスポンスされていること" do
+      get pages_main_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "ログイン失敗もしくは未ログインの場合" do
+    let(:non_correct_user) { build(:non_correct_user)}
+    before do
+      invalid_params = { email: non_correct_user.email, password: non_correct_user.password }
+      post user_session_path, params: { user: invalid_params } 
+    end
+
+    it "ステータスコード422(バリデーションエラー)がレスポンスされていること" do
+      expect(response).to have_http_status(422)
+    end
+
+    it "許可されていないページにアクセスした際、ステータスコード302（リダイレクト）がレスポンスされていること" do
+      get pages_main_path
+      expect(response).to have_http_status(302)
     end
   end
 end
