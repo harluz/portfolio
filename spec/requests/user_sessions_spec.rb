5fad5997cab561a0ede26f39d9df8c42d2e831e2
harluz <harluz06@gmail.com>
2023-04-02 09:43:37 +0900

    [clean] テストコードの体裁を整えたもの

--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -1,32 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :request do
-  describe "GET /user_sessions" do
+  describe "#new" do
     it "ログインページのgetリクエストが成功していること" do
       get new_user_session_path
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "ログインに成功する場合" do
+  describe "#create" do
+    describe "ログインに成功する場合" do
+      let(:user) { create(:user) }
+
+      before do
+        valid_params = { email: user.email, password: user.password }
+        post user_session_path, params: { user: valid_params }
+      end
+
+      it "〇〇ページにリダイレクトされていること" do
+        expect(response).to redirect_to pages_main_path
+      end
+
+      it "ステータスコード303（リダイレクト）がレスポンスされていること" do
+        expect(response).to have_http_status(303)
+      end
+
+      it "許可されているページにアクセスした際、ステータスコード200がレスポンスされていること" do
+        get pages_main_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "ログイン失敗もしくは未ログインの場合" do
+      let(:non_correct_user) { build(:non_correct_user) }
+      before do
+        invalid_params = { email: non_correct_user.email, password: non_correct_user.password }
+        post user_session_path, params: { user: invalid_params }
+      end
+
+      it "ステータスコード422(バリデーションエラー)がレスポンスされていること" do
+        expect(response).to have_http_status(422)
+      end
+
+      it "許可されていないページにアクセスした際、ステータスコード302（リダイレクト）がレスポンスされていること" do
+        get pages_main_path
+        expect(response).to have_http_status(302)
+      end
+    end
+  end
+
+  describe "#destroy" do
     let(:user) { create(:user) }
 
     before do
       valid_params = { email: user.email, password: user.password }
       post user_session_path, params: { user: valid_params }
-    end
-
-    it "〇〇ページにリダイレクトされていること" do
-      expect(response).to redirect_to pages_main_path
-    end
-
-    it "ステータスコード303（リダイレクト）がレスポンスされていること" do
-      expect(response).to have_http_status(303)
-    end
-
-    it "許可されているページにアクセスした際、ステータスコード200がレスポンスされていること" do
-      get pages_main_path
-      expect(response).to have_http_status(200)
     end
 
     it "ログアウトに成功しルートページにリダイレクトされていること" do
@@ -37,23 +65,6 @@ RSpec.describe "UserSessions", type: :request do
     it "ログアウトに成功しステータスコード303（リダイレクト）がレスポンスされていること" do
       delete destroy_user_session_path
       expect(response).to have_http_status(303)
-    end
-  end
-
-  describe "ログイン失敗もしくは未ログインの場合" do
-    let(:non_correct_user) { build(:non_correct_user) }
-    before do
-      invalid_params = { email: non_correct_user.email, password: non_correct_user.password }
-      post user_session_path, params: { user: invalid_params }
-    end
-
-    it "ステータスコード422(バリデーションエラー)がレスポンスされていること" do
-      expect(response).to have_http_status(422)
-    end
-
-    it "許可されていないページにアクセスした際、ステータスコード302（リダイレクト）がレスポンスされていること" do
-      get pages_main_path
-      expect(response).to have_http_status(302)
     end
   end
 end
