8366a239801d79200463567caa042651afccdfca
harluz <harluz06@gmail.com>
2023-04-02 09:08:13 +0900

    [update] システムスペックでdeviseのsign_inメソッドを使用できるよう設定したもの

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,6 +64,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
-  
+
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
