44c638ccc23610addd05da183667141335c43a5d
harluz <harluz06@gmail.com>
2023-04-03 20:23:11 +0900

    [add] questモデルにuser_idカラムを追加しmigrateを実行したもの

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_02_052405) do
+ActiveRecord::Schema.define(version: 2023_04_03_111116) do
 
   create_table "quests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_04_02_052405) do
     t.integer "challenge_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
