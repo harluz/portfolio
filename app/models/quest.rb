3f75d43dc44026fe5106cd644ae2d654e8b5d5d0
harluz <harluz06@gmail.com>
2023-04-03 20:24:16 +0900

    [update] userとのリレーション及びset_xpインスタンスメソッドを定義したもの

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,8 +1,15 @@
 class Quest < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true
   validate :describe
   validates :difficulty, presence: true, numericality: { only_integer: true, in: 1..5 }
   validates :xp, presence: true, numericality: { only_integer: true }
   validates :public, inclusion: { in: [true, false] }
   validate :challenge_id
+  validate :user_id
+
+  def set_xp
+    self.difficulty * 2
+  end
 end
