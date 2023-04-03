a482cb1bb0d93bb461466693bc8ea50493c4e8f3
harluz <harluz06@gmail.com>
2023-04-03 20:40:30 +0900

    [update] questのnew/createアクションを実装したもの

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,4 +1,44 @@
 class QuestsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def new
+    @quest = Quest.new
+  end
+
+  def create
+    @quest = Quest.new(quest_params)
+    @quest.user_id = current_user.id
+    @quest.xp = @quest.set_xp
+    
+    binding.pry
+    
+    if @quest.save
+      flash[:notice] = "BranChannelに新たなクエストが作成されました。"
+      redirect_to quest_path(@quest)
+    else
+      flash[:alert] = "クエストの作成に失敗しました。"
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def quest_params
+    params.require(:quest).permit(:title, :describe, :difficulty, :public)
   end
 end
