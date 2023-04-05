c03a6c0de800a491cb126a52d969361f80f54ad0
harluz <harluz06@gmail.com>
2023-04-05 21:29:24 +0900

    [update] questのファクトリを作成したもの

--- spec/factories/quests.rb
+++ spec/factories/quests.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
   factory :quest do
-    title { "MyString" }
-    describe { "MyText" }
-    difficulty { 1 }
-    xp { 1 }
+    title { "Create a quest you want to complete." }
+    describe { "Create quest achievement conditions." }
+    difficulty { 3 }
+    xp { 6 }
+    public { false }
+  end
+
+  factory :non_correct_quest, :class => "Quest" do
+    title { "" }
+    describe { "Create quest achievement conditions." }
+    difficulty { 5 }
+    xp { 10 }
     public { false }
   end
 end
