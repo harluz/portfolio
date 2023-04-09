15856bfe7cf1929ebd68747067e129264ea0af87
harluz <harluz06@gmail.com>
2023-04-09 22:20:17 +0900

    [update] updateアクションの実装及びindexアクションのデータ絞り込みを行なったもの

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,11 +1,11 @@
 class ChallengesController < ApplicationController
   def index
-    @challenges = Challenge.where(user_id: current_user.id)
+    @challenges = Challenge.where(user_id: current_user.id, close: false)
   end
 
   def new
   end
-  
+
   def create
     @challenge = Challenge.new(challenge_params)
     @quest = Quest.find(params[:challenge][:quest_id])
@@ -19,22 +19,33 @@ class ChallengesController < ApplicationController
       redirect_to challenges_path
     end
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
+    @challenge = Challenge.find(params[:id])
+    @user = User.find(@challenge.user_id)
+    @user.having_xp += @challenge.quest.xp
+    if @challenge.update(challenge_params)
+      @user.save(validate: false)
+      flash[:notice] = "クエスト達成おめでとうございます。経験値#{@challenge.quest.xp}ポイントを獲得しました。"
+      redirect_to challenges_path
+    else
+      flash[:alert] = "クエスト達成の更新ができませんでした。達成ボタンをクリックください。"
+      redirect_to challenge_path(@challenge.quest)
+    end
   end
-  
+
   def destroy
   end
 
   private
 
   def challenge_params
-    params.require(:challenge).permit(:quest_id)
+    params.require(:challenge).permit(:quest_id, :close)
   end
 end
