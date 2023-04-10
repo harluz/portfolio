5ee48aeb72acf86d1578e9188ed50c8e87ecef03
harluz <harluz06@gmail.com>
2023-04-10 21:00:27 +0900

    [update] challenge#udpateでclose:falseのパラメータが渡ってきた時の処理分岐を実装したもの

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -32,15 +32,26 @@ class ChallengesController < ApplicationController
 
   def update
     @challenge = Challenge.find(params[:id])
-    @user = User.find(@challenge.user_id)
-    @user.having_xp += @challenge.quest.xp
-    if @challenge.update(challenge_params)
-      @user.save(validate: false)
-      flash[:notice] = "クエスト達成おめでとうございます。経験値#{@challenge.quest.xp}ポイントを獲得しました。"
-      redirect_to challenges_path
-    else
-      flash[:alert] = "クエスト達成の更新ができませんでした。達成ボタンをクリックください。"
-      redirect_to challenge_path(@challenge.quest)
+    case params[:challenge][:close]
+    when "true"
+      if @challenge.update(challenge_params)
+        @user = User.find(@challenge.user_id)
+        @user.having_xp += @challenge.quest.xp
+        @user.save(validate: false)
+        flash[:notice] = "クエスト達成おめでとうございます。経験値#{@challenge.quest.xp}ポイントを獲得しました。"
+        redirect_to challenges_path
+      else
+        flash[:alert] = "クエスト達成の更新ができませんでした。達成ボタンをクリックください。"
+        redirect_to challenge_path(@challenge.quest)
+      end
+    when "false"
+      if @challenge.update(challenge_params)
+        flash[:notice] = "挑戦リストに追加しました。"
+        redirect_to challenges_path
+      else
+        flash[:alert] = "クエスト達成の更新ができませんでした。達成ボタンをクリックください。"
+        redirect_to challenge_path(@challenge.quest)
+      end
     end
   end
 
