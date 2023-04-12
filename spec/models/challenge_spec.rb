2e7bdebd23373f3305ff3e0051c795d0a9a73d65
harluz <harluz06@gmail.com>
2023-04-12 23:00:11 +0900

    [update] challenge及びquestのモデルスペックの実装及び、userのリレーションに関わるモデルスペックを追加したもの

--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Challenge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション確認" do
+    let(:user) { create(:user) }
+    let(:quest) { create(:quest, user: user) }
+    let!(:challenge) { build(:challenge,user_id: user.id, quest_id: quest.id)}
+
+    it "user_id,quest_idがある場合、有効である" do
+      expect(challenge).to be_valid
+    end
+
+    it "user_idがない場合、無効である" do
+      challenge.user_id = nil
+      challenge.valid?
+      expect(challenge.errors[:user_id]).to include("を入力してください")
+    end
+
+    it "quest_idがない場合、無効である" do
+      challenge.quest_id = nil
+      challenge.valid?
+      expect(challenge.errors[:quest_id]).to include("を入力してください")
+    end
+
+    it "closeがない場合、無効である" do
+      challenge.close = nil
+      challenge.valid?
+      expect(challenge.errors[:close]).to include("は一覧にありません")
+    end
+  end
+
+  describe "リレーション確認" do
+    let!(:association) do
+      described_class.reflect_on_association(model)
+    end
+
+    context "userとのリレーション" do
+      let(:model) { :user }
+
+      it "userとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+
+    context "challengesとのリレーション" do
+      let(:model) { :quest }
+
+      it "challengeとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+  end
 end
--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -1,5 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe Quest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション確認" do
+    let(:user) { create(:user) }
+    let(:quest) { build(:quest, user_id: user.id) }
+
+    it "title,describe,difficulty,xp,publicがある場合、有効である" do
+      expect(quest).to be_valid
+    end
+
+    it "titleがない場合、無効である" do
+      quest.title = nil
+      quest.valid?
+      expect(quest.errors[:title]).to include("を入力してください")
+    end
+
+    it "difficultyがない場合、無効である" do
+      quest.difficulty = nil
+      quest.valid?
+      expect(quest.errors[:difficulty]).to include("を入力してください")
+    end
+
+    it "difficultyが数字でない場合、無効である" do
+      quest.difficulty = "easy"
+      quest.valid?
+      expect(quest.errors[:difficulty]).to include("は数値で入力してください")
+    end
+
+    it "difficultyが整数でない場合、無効である" do
+      quest.difficulty = 0.1
+      quest.valid?
+      expect(quest.errors[:difficulty]).to include("は整数で入力してください")
+    end
+    
+    it "difficultyが0である場合、無効である" do
+      quest.difficulty = 0
+      quest.valid?
+      expect(quest.errors[:difficulty]).to include("は1以上の値にしてください")
+    end
+    
+    it "difficultyが5より大きい場合場合、無効である" do
+      quest.difficulty = 6
+      quest.valid?
+      expect(quest.errors[:difficulty]).to include("は5以下の値にしてください")
+    end
+
+    it "xpがない場合、無効である" do
+      quest.xp = nil
+      quest.valid?
+      expect(quest.errors[:xp]).to include("を入力してください")
+    end
+
+    it "xpが数字でない場合、無効である" do
+      quest.xp = "1point"
+      quest.valid?
+      expect(quest.errors[:xp]).to include("は数値で入力してください")
+    end
+
+    it "xpが整数でない場合、無効である" do
+      quest.xp = 0.1
+      quest.valid?
+      expect(quest.errors[:xp]).to include("は整数で入力してください")
+    end
+
+    it "publicがない場合、無効である" do
+      quest.public = nil
+      quest.valid?
+      expect(quest.errors[:public]).to include("は一覧にありません")
+    end
+
+    it "user_idがない場合、無効である" do
+      quest.user_id = nil
+      quest.valid?
+      expect(quest.errors[:user_id]).to include("を入力してください")
+    end
+  end
+
+  describe "リレーション確認" do
+    let!(:association) do
+      described_class.reflect_on_association(model)
+    end
+
+    context "userとのリレーション" do
+      let(:model) { :user }
+
+      it "userとの関連付けはbelongs_toであること" do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+
+    context "challengeとのリレーション" do
+      let(:model) { :challenges }
+
+      it "challengeとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :has_many
+      end
+    end
+  end
 end
