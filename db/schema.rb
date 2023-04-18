70e187900b5b0c228b1c75595e5fb6cf2be0b234
harluz <harluz06@gmail.com>
2023-04-18 21:27:29 +0900

    [update] messageモデルについてmigrateを実行したもの

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_08_141030) do
+ActiveRecord::Schema.define(version: 2023_04_18_121950) do
 
   create_table "challenges", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "quest_id"
     t.boolean "close", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -26,10 +32,10 @@ ActiveRecord::Schema.define(version: 2023_04_08_141030) do
     t.integer "difficulty", default: 1, null: false
     t.integer "xp", null: false
     t.boolean "public", default: false, null: false
-    t.integer "challenge_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "challenge_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -38,12 +44,12 @@ ActiveRecord::Schema.define(version: 2023_04_08_141030) do
     t.string "name", null: false
     t.integer "having_xp", default: 0, null: false
     t.integer "level", default: 0, null: false
-    t.integer "challenge_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "challenge_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
