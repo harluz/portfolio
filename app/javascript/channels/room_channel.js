ff4e3e726d5363b54aa1fb63fbbce4d3fd7bcd75
harluz <harluz06@gmail.com>
2023-04-20 21:52:32 +0900

    [update] roomチャネルを設定したもの

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,19 +1,30 @@
-import consumer from "./consumer"
+import consumer from "./consumer";
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+// $(document).on("turbolinks:load", function () {
+const chatChannel = consumer.subscriptions.create("RoomChannel", {
+	// connected() {
+	// Called when the subscription is ready for use on the server
+	// },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+	// disconnected() {
+	// Called when the subscription has been terminated by the server
+	// },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  },
+	received: function (data) {
+		// return alert(data['message']);
+		return $("#messages").append(data["message"]);
+	},
 
-  speak: function() {
-    return this.perform('speak');
-  }
+	speak: function (message) {
+		return this.perform("speak", { message: message });
+	},
 });
+
+$(document).on("keypress", "[data-behavior~=room_speaker]", function (event) {
+	if (event.key === "Enter") {
+		chatChannel.speak(event.target.value);
+		event.target.value = "";
+		event.preventDefault();
+	}
+});
+// });
