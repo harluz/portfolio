53102bb8ecba27d4306dba896a177aadc1643053
harluz <harluz06@gmail.com>
2023-04-22 23:15:22 +0900

    [update] room#showにおいて必要なインスタンス変数を定義したもの

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,7 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @messages = Message.all
+    @room = Room.find(params[:id])
+    @messages = @room.messages
   end
 end
