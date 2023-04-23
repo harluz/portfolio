b6b1065b684f54199a346b47ac1ecce308b869e8
harluz <harluz06@gmail.com>
2023-04-23 16:09:02 +0900

    [update] メッセージを削除できるようJSから呼び出されるdestroyアクションを実装したもの

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -10,4 +10,12 @@ class RoomChannel < ApplicationCable::Channel
   def speak(data)
     Message.create!(content: data['message'], user_id: current_user.id, room_id: params['room'])
   end
+
+  def destroy(data)
+    @message = Message.find_by(id: data['id'])
+    if @message.user == current_user
+      @message.destroy
+      ActionCable.server.broadcast "room_channel_#{params['room']}", {id: data['id']}
+    end
+  end
 end
