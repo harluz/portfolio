e0fed18e518d3d3257677e171d6c6284e5e4e690
harluz <harluz06@gmail.com>
2023-04-30 20:03:51 +0900

    [update] room及びmessageのカラムにpresence: trueを追加したもの

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,7 @@ class Message < ApplicationRecord
   belongs_to :room
 
   validates :content, presence: true
+  validates :user_id, presence: true
+  validates :room_id, presence: true
   after_create_commit { MessageBroadcastJob.perform_later self }
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :quest
   has_many :messages, dependent: :destroy
+
+  validates :quest_id, presence: true
 end
