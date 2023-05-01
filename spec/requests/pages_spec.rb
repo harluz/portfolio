bc042470149466d9bd60c22efec95d34984f49c9
harluz <harluz06@gmail.com>
2023-05-01 12:08:29 +0900

    [update] ユーザーのプロフィールページに関するシステム・リクエストスペックを実装したもの

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -7,4 +7,46 @@ RSpec.describe "Pages", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET #profile" do
+    let!(:user) { create(:user) }
+    let!(:quest1) { create(:quest, user: user) }
+    let!(:quest2) { create(:quest, user: user) }
+    let!(:quest3) { create(:quest, user: user) }
+    let(:challenge1) { create(:challenge, user: user, quest: quest1) }
+    let(:challenge2) { create(:challenge, user: user, quest: quest2) }
+    subject { response.body }
+
+    context "ユーザーがログインしている場合" do
+      before do
+        sign_in user
+        get pages_profile_path
+      end
+
+      it "プロフィールページのアクセスが成功すること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "ユーザー名、達成クエスト数、作成クエスト数、獲得経験値がレスポンスに含まれていること" do
+        challenge1.close = true
+        challenge2.close = true
+        expect(subject).to include user.name
+        expect(subject).to include "2"
+        expect(subject).to include "3"
+        expect(subject).to include "100"
+      end
+    end
+
+    context "ユーザーがログインしていない場合" do
+      before { get pages_profile_path }
+
+      it "ステータスコード302（リダイレクト）がレスポンスされていること" do
+        expect(response).to have_http_status(302)
+      end
+
+      it "sign_inページにリダイレクトするレスポンスが含まれていること" do
+        expect(subject).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -1,9 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :system do
-  before do
-    driven_by(:rack_test)
+  let!(:user) { create(:user) }
+  describe "表示確認" do
+    context "/pages/profile" do
+      let!(:quest1) { create(:quest, user: user) }
+      let!(:quest2) { create(:quest, user: user) }
+      let!(:quest3) { create(:quest, user: user) }
+      let!(:challenge1) { create(:challenge, user: user, quest: quest1) }
+
+      before do
+        sign_in user
+        visit challenges_path
+        click_on "達成"
+        visit pages_profile_path
+      end
+
+      it "プロフィール情報が表示されていること" do
+        expect(page).to have_content "ユーザー名"
+        expect(page).to have_content "達成クエスト数(再挑戦を含む)"
+        expect(page).to have_content "作成クエスト数"
+        expect(page).to have_content "獲得経験値"
+        expect(page).to have_content user.name
+        expect(page).to have_content "1"
+        expect(page).to have_content "3"
+        expect(page).to have_content "106"
+      end
+
+      it "数値が３桁区切りとなっていること" do
+        user.challenge_achieved = 1000
+        user.having_xp = 1000000
+        user.save
+        visit pages_profile_path
+        expect(page).to have_content "1,000"
+        expect(page).to have_content "1,000,000"
+      end
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "ページ遷移確認" do
+
+  end
 end
