6500c6940d20bea1244b726e5ce80e397e75afed
harluz <harluz06@gmail.com>
2023-05-01 12:02:18 +0900

    [update] ユーザーのプロフィール及び退会に関するルーティング及びコントローラーを定義したもの

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, except: [:top]
+
   def top
   end
 
   def main
+  end
+
+  def profile
+    @user = current_user
+  end
+
+  def withdraw
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'pages#top'
   get 'pages/top'
   get 'pages/main'
+  get 'pages/profile'
+  get 'pages/withdraw'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
     registrations: 'users/registrations',
