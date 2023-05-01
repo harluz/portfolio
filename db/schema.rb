fd9887c16206e0ca0ad8874967932cad4811b1c3
harluz <harluz06@gmail.com>
2023-05-01 12:04:08 +0900

    [add && update] プロフィールに表示するクエスト達成数を保存するchallenge_achievedカラムを追加したもの

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_115441) do
+ActiveRecord::Schema.define(version: 2023_05_01_013947) do
 
   create_table "challenges", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2023_04_21_115441) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "challenge_achieved", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
