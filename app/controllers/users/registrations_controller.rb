f368080964b6b16ca45fc9d9e85d7e9b4afe40b3
harluz <harluz06@gmail.com>
2023-05-12 23:04:57 +0900

    [change] ユーザー登録・ログイン後のリダイレクト先をクエスト一覧ページに変更したもの

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    pages_main_path
+    quests_path
   end
 
   def after_update_path_for(resource)
