bbaf678a0bc8c13ac09c52b6b15176ac40146616
harluz <harluz06@gmail.com>
2023-05-20 10:20:36 +0900

    [update] ActionCableに関わるテストを追加したもの

--- spec/channels/room_channel_spec.rb
+++ spec/channels/room_channel_spec.rb
@@ -4,43 +4,70 @@ RSpec.describe RoomChannel, type: :channel do
   let!(:user) { create(:user) }
   let!(:quest) { create(:quest, user: user) }
   let!(:room) { create(:room, id: 1, quest_id: quest.id) }
-  let!(:message) { create(:message, user: user, room: room) }
-
-  before do
-    stub_connection current_user: user
-    subscribe(room: room.id)
-  end
-
-  it 'room_channelに接続されていること' do
-    expect(subscription).to be_confirmed
-  end
-
-  it 'メッセージを作成し、正しいチャネルにブロードキャストされていること' do
-    expect {
-      perform(:speak, message: 'Hello, world!')
-      sleep 3
-    }.to have_broadcasted_to("room_channel_#{room.id}")
-       .with { |data| expect(data[:message]).to match(/Hello, world!/) }
-  end
-
-  it 'speakアクションでmessageが作成されていること' do
-    expect {
-      perform(:speak, message: 'Hello, world!')
-    }.to change(Message, :count).by(1)
-
-    expect(Message.last.content).to eq('Hello, world!')
-    expect(Message.last.user_id).to eq(user.id)
-    expect(Message.last.room_id).to eq(room.id)
-  end
-
-  it 'destroyアクションでmessageが削除されていること' do
-    expect {
-      perform(:destroy, id: message.id)
-    }.to change(Message, :count).by(-1)
   
-    expect(Message.find_by(id: message.id)).to be_nil
-    expect {
-      perform(:destroy, id: 0)
-    }.not_to change(Message, :count)
+  context "speak action" do
+    let(:other_user) { create(:correct_user) }
+    let(:message) { create(:message, user: other_user, room: room) }
+    before do
+      stub_connection current_user: user
+      subscribe(room: room.id)
+    end
+  
+    it 'room_channelに接続されていること' do
+      expect(subscription).to be_confirmed
+    end
+
+    it 'speakアクションでmessageが作成されていること' do
+      expect {
+        perform(:speak, message: 'Hello, world!')
+      }.to change(Message, :count).by(1)
+  
+      expect(Message.last.content).to eq('Hello, world!')
+      expect(Message.last.user_id).to eq(user.id)
+      expect(Message.last.room_id).to eq(room.id)
+    end
+  
+    it 'メッセージを作成し、正しいチャネルにブロードキャストされていること' do
+      expect {
+        perform(:speak, message: 'Hello, world!', room_id: room.id)
+        sleep 3
+      }.to have_broadcasted_to("room_channel_#{room.id}")
+        .with { |data| expect(data["messagecurrent"]).to match(/Hello, world!/) }
+    end
+
+    it '他のユーザーのメッセージがブロードキャストされていること' do
+      expect {
+        perform(:destroy, id: message.id)
+        sleep 3
+      }.to have_broadcasted_to("room_channel_#{room.id}")
+        .with { |data| expect(data['messageother']).to include(message.content) }
+    end
+  end
+
+  context "destroy ation" do
+    let!(:message) { create(:message, user: user, room: room) }
+    before do
+      stub_connection current_user: user
+      subscribe(room: room.id)
+    end
+
+    it 'メッセージの削除がブロードキャストされていること' do
+      expect {
+        perform(:destroy, id: message.id)
+        sleep 3
+      }.to have_broadcasted_to("room_channel_#{room.id}")
+        .with { |data| expect(data['id']).to eq(message.id) }
+    end
+
+    it 'destroyアクションでmessageが削除されていること' do
+      expect {
+        perform(:destroy, id: message.id)
+      }.to change(Message, :count).by(-1)
+    
+      expect(Message.find_by(id: message.id)).to be_nil
+      expect {
+        perform(:destroy, id: 0)
+      }.not_to change(Message, :count)
+    end
   end
 end
