4de124ecc23b27980836f496c96e55b0d408ab18
harluz <harluz06@gmail.com>
2023-05-21 12:56:00 +0900

    [update] トークルームにアクセスした時にメッセージスクロール位置を最下部になるよう修正したもの

---
+++ app/javascript/javascripts/application.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+	var messageContainer = document.getElementById("message-container");
+	messageContainer.scrollTop = messageContainer.scrollHeight;
+});
